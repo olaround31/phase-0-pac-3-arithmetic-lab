86f3ac7394233508d95310cbfa678d365fe7eb60
JohnMichaelOLavidez <Olaround31@gmail.com>
2023-03-06 11:34:09 -0500

    Adds myfile.md

---
+++ index.js
@@ -0,0 +1,57 @@
+function add(){
+   // 
+}
+function subtract(){
+    //
+}
+function multiply(){
+    //
+}
+function divide(){
+    //
+}
+function add (_a,_b){
+    return "something";
+}
+function add (a,b){
+    return a + b;
+}
+function subtract (_a,_b){
+    return "something";
+}
+function subtract (a,b){
+    return a - b ;
+}
+function multiply (a,b){
+    return a + b;
+}
+function multiply (_a,_b){
+    return "something";
+}
+function multiply (a,b){
+    return a * b ;
+}
+function divide (a,b){
+    return a + b;
+}
+function divide (_a,_b){
+    return "something";
+}
+function divide (a,b){
+    return a / b ;
+}
+{
+    a=3;
+}
+function increment(a){
+    return ++a;
+}
+function decrement(a){
+    return --a;
+}
+function makeInt(a){
+ return(parseInt(a,"10"));
+} 
+function preserveDecimal(a){
+return(parseFloat(a,10));
+}
